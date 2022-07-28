4deede07c7906ddfa86d6a4451c9b59af67d464c
idelahera <idelahera8@gmail.com>
2022-07-28 09:41:00 +0200

    Change background color of gridSquare on hover

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 // Select the elements in the DOM
 const mainGrid = document.getElementById("mainGrid")
 
-let size = 5
+let size = 50
 
 // Create all gridSquares needed
 for(let i = 0; i < size*size; i++) {
     let gridSquare = document.createElement("div")
+    gridSquare.addEventListener("mouseenter", function(){
+        this.style.backgroundColor = "black"
+    })
     mainGrid.appendChild(gridSquare)
 }
 
