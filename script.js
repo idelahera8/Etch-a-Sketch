1d0229c1bd488c708e3904a895d35500dc5c033e
idelahera <idelahera8@gmail.com>
2022-07-28 10:08:33 +0200

    Clear grid on button click

--- script.js
+++ script.js
@@ -1,16 +1,44 @@
-// Select the elements in the DOM
+// Select the mainGrid in the DOM
 const mainGrid = document.getElementById("mainGrid")
 
+// Select the buttons in the DOM
+const clearButton = document.getElementById("clearButton")
+
+// Start all variables
 let size = 50
+// let mouseClicked = false
+
+// Start the grid
+mainGrid.style.display = "grid"
+mainGrid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
 
 // Create all gridSquares needed
 for(let i = 0; i < size*size; i++) {
     let gridSquare = document.createElement("div")
+    gridSquare.classList.add("gridSquare")
+
+    // 
+ //   gridSquare.addEventListener("mousedown", function(){
+//        mouseClicked = true
+//        console.log(mouseClicked)
+//    })
+
+//    gridSquare.addEventListener("mouseup", function(){
+//        mouseClicked = false
+//        console.log(mouseClicked)
+//    })
+
     gridSquare.addEventListener("mouseenter", function(){
-        this.style.backgroundColor = "black"
+//        if (mouseClicked) {
+            this.style.backgroundColor = "black"
+ //       }
+        
     })
     mainGrid.appendChild(gridSquare)
 }
 
-mainGrid.style.display = "grid"
-mainGrid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+// Clear grid
+clearButton.addEventListener("click", function(){
+    let allGridSquares = document.querySelectorAll(".gridSquare")
+    allGridSquares.forEach(gridS => gridS.style.backgroundColor = "white")
+})
